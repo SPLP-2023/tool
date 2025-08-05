70615dda4669844d785c69d0223be606222c137e
SPLP-2023 <Luke.storey@strikepoint.uk>
2025-08-05 15:09:08 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 const CACHE_NAME = 'strikeR-cache-v1';
 
 const urlsToCache = [
-  './',
-  './index.html',
-  './reports.html', 
-  './survey.html',
-  './t&i-report.html',
-  './risk-assessment.html',
-  './tools.html',
-  './css/styles.css',
-  './js/app.js',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/reports.html', 
+  '/survey.html',
+  '/t&i-report.html',
+  '/risk-assessment.html',
+  '/tools.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
